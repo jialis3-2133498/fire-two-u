d249846a36679624aad6c8dad55e2cf81bf1131a
jiali_sheng <jialisheng2002@gmail.com>
2026-04-01 19:32:59 -0700

    Update logistic model and main() file

---
+++ main.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from src.logistic_models import (
+    create_horizon_label,
+    train_all_logistic_models,
+    predict_all_horizons,
+    evaluate_predictions
+    )
+
+
+def main():
+    # 1. load data
+    train_df = pd.read_csv("fire-two-u/data/train_split.csv")
+    valid_df = pd.read_csv("fire-two-u/data/val_split.csv")
+    test_df = pd.read_csv("fire-two-u/data/test_clean.csv")
+    # 2. create labels
+    train_df = create_horizon_label(train_df)
+    valid_df = create_horizon_label(valid_df)
+    # 3. define feature_cols and target_cols
+    feature_cols = [
+        c for c in train_df.columns
+        if c not in [
+            "event_id",
+            "event",
+            "time_to_hit_hours",
+            "y_12",
+            "y_24",
+            "y_48",
+            "y_72"
+        ]
+    ]
+    # feature_cols = train_df[feature_cols].select_dtypes(
+    #     include="number").columns.tolist()
+    target_cols = ["y_12", "y_24", "y_48", "y_72"]
+    # 4. train models
+    models = train_all_logistic_models(
+        train_df, valid_df, feature_cols, target_cols)
+    # 5. predict
+    predictions = predict_all_horizons(models, valid_df, feature_cols)
+    # 6. evaluate
+    model_evaluation = {}
+    for each_key in predictions.keys():
+        y_prob = predictions[each_key]
+        auc, report = evaluate_predictions(
+            valid_df[each_key], y_prob)
+        model_evaluation[each_key] = {
+            "auc": auc,
+            "report": report
+        }
+
+        print(f"\n=== {each_key} ===")
+        print("AUC:", auc)
+        print(report)
+    # 7. Predictions on test file
+    test_predictions = predict_all_horizons(models, test_df, feature_cols)
+    return model_evaluation, test_predictions
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/logistic_models.py
@@ -0,0 +1,109 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, classification_report
+
+
+def create_horizon_label(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    df["y_12"] = ((df["event"] == 1) & (df["time_to_hit_hours"] <= 12)).astype(int)
+    df["y_24"] = ((df["event"] == 1) & (df["time_to_hit_hours"] <= 24)).astype(int)
+    df["y_48"] = ((df["event"] == 1) & (df["time_to_hit_hours"] <= 48)).astype(int)
+    df["y_72"] = ((df["event"] == 1) & (df["time_to_hit_hours"] <= 72)).astype(int)
+
+    return df
+
+
+def train_logistic_model(
+        train_df: pd.DataFrame,
+        valid_df: pd.DataFrame,
+        feature_cols: list[str],
+        target_col: str,
+        random_state: int = 42
+        ):
+    X_train = train_df[feature_cols]
+    y_train = train_df[target_col]
+
+    X_valid = valid_df[feature_cols]
+    y_valid = valid_df[target_col]
+
+    # SimpleImputer() fills out missing value in the dataset
+    imputer = SimpleImputer(strategy="median")
+    X_train_imputed = imputer.fit_transform(X_train)
+    X_valid_imputed = imputer.transform(X_valid)
+
+    # StandardScaler() standardizes each feature so that they have
+    # a similar scale, usually mean at 0, and std at 1.
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train_imputed)
+    X_valid_scaled = scaler.transform(X_valid_imputed)
+
+    model = LogisticRegression(
+        max_iter=1000,
+        class_weight="balanced",
+        random_state=random_state)
+    model.fit(X_train_scaled, y_train)
+
+    y_prob = model.predict_proba(X_valid_scaled)[:, 1]
+    auc = roc_auc_score(y_valid, y_prob)
+    return model, imputer, scaler, auc
+
+
+def train_all_logistic_models(
+        train_df: pd.DataFrame,
+        valid_df: pd.DataFrame,
+        feature_cols: list[str],
+        target_cols: list[str]):
+    model_dict = {}
+    for each_target_col in target_cols:
+        model, imputer, scaler, auc = train_logistic_model(
+            train_df, valid_df, feature_cols, each_target_col)
+        model_dict[each_target_col] = {
+            "model": model,
+            "imputer": imputer,
+            "scaler": scaler,
+            "auc": auc
+        }
+    return model_dict
+
+
+def predict_logistic_model(
+        model,
+        imputer,
+        scaler,
+        df: pd.DataFrame,
+        feature_cols: list[str]
+):
+    X = df[feature_cols]
+    X_imputed = imputer.transform(X)
+    X_scaled = scaler.transform(X_imputed)
+    y_prob = model.predict_proba(X_scaled)[:, 1]
+    return y_prob
+
+
+def predict_all_horizons(
+        fitted_models: dict,
+        df: pd.DataFrame,
+        feature_cols: list[str]
+):
+    prediction_dict = {}
+    for each_key in fitted_models.keys():
+        each_model = fitted_models[each_key]["model"]
+        each_imputer = fitted_models[each_key]["imputer"]
+        each_scaler = fitted_models[each_key]["scaler"]
+        y_prob = predict_logistic_model(each_model,
+                                        each_imputer,
+                                        each_scaler,
+                                        df,
+                                        feature_cols)
+        prediction_dict[each_key] = y_prob
+    return prediction_dict
+
+
+def evaluate_predictions(y_true, y_prob):
+    y_pred = (y_prob >= 0.5).astype(int)
+    auc = roc_auc_score(y_true, y_prob)
+    report = classification_report(y_true, y_pred, output_dict=True)
+    return auc, report
