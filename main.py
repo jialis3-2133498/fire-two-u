33e0e2533029cd54e1b8687e6aa6f10382686cec
yifanl26 <46547330+yifanl26@users.noreply.github.com>
2026-04-02 18:33:54 -0700

    add histogram

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from src.logistic_models import (
     create_horizon_label,
     train_all_logistic_models,
     predict_all_horizons,
-    evaluate_predictions
-    )
+    evaluate_predictions,
+)
 from src.data_visualization import plot_target_balance
+from src.data_visualization import plot_feature_histograms
 
 
 def main():
@@ -19,36 +20,34 @@ def main():
 
     # 3. define feature_cols and target_cols
     feature_cols = [
-        c for c in train_df.columns
-        if c not in [
+        c
+        for c in train_df.columns
+        if c
+        not in [
             "event_id",
             "event",
             "time_to_hit_hours",
             "y_12",
             "y_24",
             "y_48",
-            "y_72"
+            "y_72",
         ]
     ]
     # feature_cols = train_df[feature_cols].select_dtypes(
     #     include="number").columns.tolist()
     target_cols = ["y_12", "y_24", "y_48", "y_72"]
     plot_target_balance(train_df, target_cols)
+    plot_feature_histograms(train_df, feature_cols)
     # 4. train models
-    models = train_all_logistic_models(
-        train_df, valid_df, feature_cols, target_cols)
+    models = train_all_logistic_models(train_df, valid_df, feature_cols, target_cols)
     # 5. predict
     predictions = predict_all_horizons(models, valid_df, feature_cols)
     # 6. evaluate
     model_evaluation = {}
     for each_key in predictions.keys():
         y_prob = predictions[each_key]
-        auc, report = evaluate_predictions(
-            valid_df[each_key], y_prob)
-        model_evaluation[each_key] = {
-            "auc": auc,
-            "report": report
-        }
+        auc, report = evaluate_predictions(valid_df[each_key], y_prob)
+        model_evaluation[each_key] = {"auc": auc, "report": report}
 
         print(f"\n=== {each_key} ===")
         print("AUC:", auc)
